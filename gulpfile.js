d03b9aa55733bb77000dc6781840151bdc931a19
Penny Lyle <pennylyle-online@yahoo.com>
2020-10-24 18:51:20 +0200

    Set up gulp tasks and sass, add git ignore

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,32 @@
-function defaultTask(cb) {
-    // place code for your default task here
-    cb();
-}
+var browserSync = require('browser-sync').create;
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sassPath = 'app/sass/**/*.+(sass|scss)';
 
-exports.default = defaultTask;
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: {
+            baseDir: 'app'
+        },
+    });
+});
+
+gulp.task('sass', function () {
+    return gulp.src(sassPath)
+        .pipe(sass())
+        .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
+});
+
+gulp.task('watch', function () {
+    gulp.watch(sassPath, gulp.series(['sass']));
+});
+
+// function defaultTask(cb) {
+//     // place code for your default task here
+//     cb();
+// }
+
+// exports.default = defaultTask;
